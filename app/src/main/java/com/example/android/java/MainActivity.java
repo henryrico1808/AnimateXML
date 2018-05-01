336eccdf470abd105251c239570d8e9727e58604
henryrico1808 <36237658+henryrico1808@users.noreply.github.com>
2018-05-01 17:04:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/java/MainActivity.java
@@ -0,0 +1,75 @@
+//android:onClick="onButtonClick"
+
+package com.example.android.java;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button btngira,btnrun;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.faceIcon);
+
+    }
+
+    public void onButtonClick(final View v) {
+        Animation animation =
+                AnimationUtils.loadAnimation(this, R.anim.grow);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setScaleX(2);
+                imageView.setScaleY(2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+
+    public void onClick(final View v) {
+        Animation animation2 =
+                AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        animation2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setScaleX(2);
+                imageView.setScaleY(2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation2);
+    }
+
+}
